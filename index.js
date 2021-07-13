5b879cdefb1de3e346921b706b391462e9314f27
Arun N A <arun92santhi@gmail.com>
2021-07-13 14:33:13 +0530

    audio getByteFrequencyData

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 8000;
+var count = 0
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -11,12 +12,17 @@ io.on('connection', (socket) => {
   socket.on('audio frames', msg => {
     var audioFrameIs = new Uint8Array(msg);
     console.log(audioFrameIs);
+    // call socekt emit here
   });
   socket.on('video frames', msg => {
-    console.log("Videooooo*****: ", msg)
-    var videoFrameIs = new Uint8ClampedArray(msg);
-    socket.emit("rec", videoFrameIs);
-    console.log(videoFrameIs);
+    // console.log("Videooooo*****: ", msg)
+    count += 1
+    if (count>=10) {
+      var videoFrameIs = new Uint8ClampedArray(msg);
+      socket.emit("rec", videoFrameIs);
+      console.log(videoFrameIs);
+      count = 0
+    }
   });
 });
 
